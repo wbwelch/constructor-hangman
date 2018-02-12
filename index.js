38c75313957e921d61b9beaa7e182d4d5103eeab
WB Welch <wbwelch@wbs-macbook-pro.attlocal.net>
2018-02-11 18:43:53 -0600

    Letter.js work

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var inquirer = require('inquirer');
-var word = require('./Word.js');
-var letter = require('./Letter.js');
+
+var word = require('./lib/Word.js');
+var letter = require('./lib/Letter.js');
+var words = require('./lib/words.js');
+
 var lives = 10;
 var userGuess = null;
 var gameWord = null;
 
 var game = {
-	//stores random words
-	gameWords: ['Flower Power', 'Groovy', 'Peace Out Man', 'All You Need is Love', 'The Beatles', 'Make Love, Not War', 'Far Out', 'Peace', 'Hippie', 'Bell Bottoms', 'Don\'t Worry', 'Jimi Hendrix', 'Grateful Dead', 'Janis Joplin', 'Lava Lamp', 'Led Zeppelin', 'The Doors'],
+	
+	
 	//selects random word and uses word constructor to store it
 	wordSelect: function() {
 		for (var i = 0; i < game.gameWords.length; i++) {
-			gameWord = game.gameWords[Math.floor(Math.random() * game.gameWords.length)];
+			gameWordTemp = game.gameWords[Math.floor(Math.random() * game.gameWords.length)];
+			gameWord = gameWordTemp.toString();
 			word.gameWord = gameWord;
 			letter.gameWord = gameWord;
-			letter.Letter.newWord();
+//			letter.letterPerm[i].newWord();
+			//console.log(letter);
 		};
 	},
 	//promtps the user for each guess
@@ -27,20 +32,25 @@ var game = {
 		  	}
 		]).then(function(guess){
 			if(lives > 0) {
-				letter.letterPerm.checkInput();
-				if (letter.letterPerm.guessed === false) {
-					lives--;
-					console.log('You have ' + lives + ' lives remaining.');
-				}
-				else {
-					letter.letterPerm.printLetter();
-					console.log('\nYou have ' + lives + ' lives remaining.')
+				for (var i = 0; i < gameWord.length; i++) {
+					letter.Letter[i].checkInput();
+					if (letter.letterPerm[i].guessed === false) {
+						lives--;
+						console.log('You have ' + lives + ' lives remaining.');
+					}
+					else {
+						letter.letterPerm[i].printLetter();
+						console.log('\nYou have ' + lives + ' lives remaining.')
+					};
 				};
 			};
 		});
 	}
 };
 game.wordSelect();
+console.log(letter);
+console.log(word);
+//Letter.newWord();
 game.promptUser();
 
 //need to code in game reset
